555bb8f5de152b30542e6a8b1ed5d6705d49a798
g-sierra <193622299+g-sierra@users.noreply.github.com>
2025-08-11 22:15:06 -0300

    Se solucionó el problema al hacer click en el botón

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 const div = document.querySelector(".btn-container");
+const btn = document.getElementById("btn");
 
 div.addEventListener("click", () => {
     alert("Hola! Soy el div");
+});
+
+btn.addEventListener("click", (event) => {
+    event.stopPropagation();
+    alert("Hola!");
 });
